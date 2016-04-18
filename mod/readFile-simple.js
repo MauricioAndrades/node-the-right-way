f2023310b0e8dda9c82ae5dd0e596938414b7374
Mauricio Andrades <mod.and.op@gmail.com>
2016-04-18 03:52:24 -0700

    added target readfile

--- mod/readFile-simple.js
+++ mod/readFile-simple.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const fs = require('fs');
+/** If readFile() is successful, then err will be false. Otherwise the err parameter will contain an Error object */
+/** uncaught exception in Node will halt the program by escaping the event loop */
 fs.readFile('target.txt', function(err, data) {
   if (err) {
     throw err;
