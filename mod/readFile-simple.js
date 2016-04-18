9d1c92b80ae31652abe1b36e08d7d96a97b5c56d
Mauricio Andrades <mod.and.op@gmail.com>
2016-04-18 03:53:49 -0700

    fixed broken path from project root

--- mod/readFile-simple.js
+++ mod/readFile-simple.js
@@ -3,7 +3,8 @@
 const fs = require('fs');
 /** If readFile() is successful, then err will be false. Otherwise the err parameter will contain an Error object */
 /** uncaught exception in Node will halt the program by escaping the event loop */
-fs.readFile('target.txt', function(err, data) {
+/** data is a buffer */
+fs.readFile('mod/target.txt', function(err, data) {
   if (err) {
     throw err;
   }
