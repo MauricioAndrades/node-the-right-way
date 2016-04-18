f604adff03075888aef385fac40f12647f6c4dac
Mauricio Andrades <mod.and.op@gmail.com>
2016-04-18 03:37:37 -0700

    more comments

--- mod.js
+++ mod.js
@@ -18,13 +18,17 @@ fs.watch(filename, function() {
   /** output @type {buffer} [buffer for outputstream] */
   let output = '';
 
-  /**
-   *  Event listeners. Callback invoked on event dispatch. Since the Stream class inherits from EventEmitter, we can listen for events from the child process’s standard output stream.
-   */
+  /** Event listeners. Callback invoked on event dispatch. Since the Stream class inherits from EventEmitter, we can listen for events from the child process’s standard output stream. */
+
+  /** A Buffer is Node’s way of representing binary data. It points to a blob of memory allocated by Node’s native core, outside of the JavaScript engine. Buffers can’t be resized and they require encoding and decoding to convert to and from JavaScript strings. */
+
   ls.stdout.on('data', function(chunk) {
     output += chunk.toString();
   })
 
+  /** Like Stream, the ChildProcess class extends EventEmitter, so we can add listeners to it, as well. */
+
+  /** After a child process has exited and all its streams have been flushed, it emits a close event. When the callback printed here is invoked, we parse the output data by splitting on sequences of one or more whitespace characters (using the regular expression /\s+/). Finally, we use console.dir() to report on the first, fifth, and ninth fields (indexes 0, 4, and 8), which correspond to the permis- sions, size, and file name, respectively. */
   ls.on('close', function() {
     let parts = output.split(/\s+/);
     console.dir([parts[0], parts[4], parts[8]]);
