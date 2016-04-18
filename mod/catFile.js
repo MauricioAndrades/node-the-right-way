1fdae407d6c9a4dafc6c7bfefd3a40715fd912e6
Mauricio Andrades <mod.and.op@gmail.com>
2016-04-18 04:07:39 -0700

    added readstream --harmony

--- mod/catFile.js
+++ mod/catFile.js
@@ -2,3 +2,7 @@
 require('fs').createReadStream(process.argv[2]).pipe(process.stdout);
 
 /** to run it: ./cat.js <file_name> */
+
+/** from root: mod/catFile.js app.js */
+
+/** The code in cat.js does not bother assigning the fs module to a variable. The require() function returns a module object, so we can call methods on it directly. */
